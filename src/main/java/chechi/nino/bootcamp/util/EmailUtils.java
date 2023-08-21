12fe4a195a273375ccdaecf8263e896595150d7a
xChechi <stefannasev81@gmail.com>
2023-08-21 11:47:03 +0100

    Forgotten Password Service exposure fix

--- src/main/java/chechi/nino/bootcamp/util/EmailUtils.java
+++ src/main/java/chechi/nino/bootcamp/util/EmailUtils.java
@@ -1,18 +1,30 @@
 package chechi.nino.bootcamp.util;
 
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
+@Component
 public class EmailUtils {
+
+    private final String fromEmail;
+    private final String password;
+
+    @Autowired
+    public EmailUtils(String fromEmail, String emailPassword) {
+        this.fromEmail = fromEmail;
+        this.password = emailPassword;
+    }
 
     public static void sendPasswordEmail(String toEmail, String newPassword) {
 
         //Manually added javax.mail to project structure. Jar file is in lib folder
-
-        String fromEmail = "sirenal@abv.bg";
-        String password = "ccdccd12";
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
@@ -23,13 +35,14 @@ public class EmailUtils {
         Session session = Session.getInstance(props, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(fromEmail, password);
+                return null;
+                //return new PasswordAuthentication(fromEmail, password);
             }
         });
 
         try {
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(fromEmail));
+            //message.setFrom(new InternetAddress(fromEmail));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
             message.setSubject("Password Reset");
             message.setText("Your new password: " + newPassword);
