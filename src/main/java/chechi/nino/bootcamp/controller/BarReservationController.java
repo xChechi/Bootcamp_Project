73835f5841ceb3a8de685280fece9fb182aa9ac0
xChechi <stefannasev81@gmail.com>
2023-09-18 10:25:41 +0100

    Bar Reservation Service and Controller added

--- src/main/java/chechi/nino/bootcamp/controller/BarReservationController.java
+++ src/main/java/chechi/nino/bootcamp/controller/BarReservationController.java
@@ -5,11 +5,13 @@ import chechi.nino.bootcamp.dto.reservation_bar.BarReservationResponse;
 import chechi.nino.bootcamp.service.BarReservationService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -40,5 +42,52 @@ public class BarReservationController {
     public ResponseEntity<BarReservationResponse> bookBarReservation (@PathVariable Integer userId, @Valid @RequestBody BarReservationRequest request) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(barReservationService.bookBarReservation(userId, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBarReservationById (@PathVariable Integer id) {
+
+        barReservationService.DeleteBarReservationById(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BarReservationResponse> updateBarReservation (@PathVariable Integer id, @Valid @RequestBody BarReservationRequest request) {
+
+        BarReservationResponse response = barReservationService.updateBarReservation(id, request);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping(params = "userId")
+    public ResponseEntity<List<BarReservationResponse>> searchBarReservationByUser (@RequestParam("userId") Integer userId) {
+
+        List<BarReservationResponse> responses = barReservationService.searchByUser(userId);
+
+        if (responses.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } else return ResponseEntity.status(HttpStatus.FOUND).body(responses);
+    }
+
+    @GetMapping(params = "seatId")
+    public ResponseEntity<List<BarReservationResponse>> searchBarReservationBySeat (@RequestParam("seatId") Integer seatId) {
+
+        List<BarReservationResponse> responses = barReservationService.searchBySeat(seatId);
+
+        if (responses.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } else return ResponseEntity.status(HttpStatus.FOUND).body(responses);
+    }
+
+    @GetMapping(params = "date")
+    public ResponseEntity<List<BarReservationResponse>> searchBarReservationByDate (
+            @RequestParam("reservationDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime date) {
+
+        List<BarReservationResponse> responses = barReservationService.searchByDate(date);
+
+        if (responses.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else return ResponseEntity.status(HttpStatus.FOUND).body(responses);
     }
 }
