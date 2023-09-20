259f19396695bb281544d7c90d4875856f377bb6
xChechi <stefannasev81@gmail.com>
2023-09-20 09:30:40 +0100

    Project Security implementation - User Details & JwtService added

--- src/main/java/chechi/nino/bootcamp/entity/user/User.java
+++ src/main/java/chechi/nino/bootcamp/entity/user/User.java
@@ -14,8 +14,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -28,7 +32,7 @@ import java.util.List;
         @UniqueConstraint(columnNames = "last_name"),
         @UniqueConstraint(columnNames = "email")
 })
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -75,4 +79,33 @@ public class User {
     private List<CarReservation> carReservationList;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
