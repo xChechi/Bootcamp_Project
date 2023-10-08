f462faf4c34d70707eb467a909ad9de9a72d50c2
xChechi <stefannasev81@gmail.com>
2023-10-08 17:48:43 +0100

    Contact Us Controller added

--- src/main/java/chechi/nino/bootcamp/controller/html/ContactController.java
+++ src/main/java/chechi/nino/bootcamp/controller/html/ContactController.java
@@ -1,18 +1,31 @@
 package chechi.nino.bootcamp.controller.html;
 
 import chechi.nino.bootcamp.dto.contact.ContactUsRequest;
+import chechi.nino.bootcamp.service.ContactUsService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api/v1/contact")
+@AllArgsConstructor
 public class ContactController {
+
+    private final ContactUsService contactUsService;
 
     @GetMapping
     public String contact (Model model) {
         model.addAttribute("contactUsRequest", new ContactUsRequest());
+        return "contact";
+    }
+
+    @PostMapping
+    public String sendMessage (ContactUsRequest request, Model model) {
+        contactUsService.saveMessage(request);
+        model.addAttribute("message", true);
         return "contact";
     }
 
